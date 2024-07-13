1b8e8c2ec4762fdf073fbb88521c7fb4bc099ce9
ooker <ganuongphap@gmail.com>
2024-07-13 17:25:37 +0700

    thêm slide background trấn kỳ

---
+++ watch.ps1
@@ -0,0 +1,6 @@
+node 'D:\Programming\Theo nhu cầu\Slide\reveal-md\bin\reveal-md.js' . `
+  --absolute-url https://slide.quảcầu.cc --featured-slide 1 `
+  --watch `
+  --template template/slide.html `
+  --listing-template template/listing.html `
+  # --disable-auto-open
